90877b789be88c7ce4728200573376c1cba31f38
Subramanyam R <subramanyam.r@searce.com>
2022-12-28 14:40:54 +0530

    Add cart testing

--- sql_commands/testing_state.sql
+++ sql_commands/testing_state.sql
@@ -12,4 +12,8 @@ INSERT INTO product VALUES(1, 'Hoodie', '{"color": "black", "gender": "male"}', 
 INSERT INTO product VALUES(2, 'TWS', '{"color": "white", "ANC": "true"}', 65546, 2, 14.99);
 INSERT INTO product VALUES(3, 'Jeans', '{"color": "Jean Blue"}', 65547, 1, 25.99);
 
-INSERT INTO inventory VALUES(3, 100)
+INSERT INTO inventory VALUES(3, 100);
+
+INSERT INTO cart_item VALUES('4d4d8297-7663-451d-b79e-49a545728552', 3, 10);
+
+INSERT INTO cart_reference VALUES('4d4d8297-7663-451d-b79e-49a545728552', '2022-12-28 13:52:51.016582');
---
+++ tests/AddItemsToCart_test.go
@@ -0,0 +1,50 @@
+package tests
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/subramanyam-searce/product-catalog-go/helpers"
+)
+
+func AddToCartViaAPI(ref string, product_id int, quantity int, t *testing.T) map[string]string {
+	request_url := fmt.Sprintf("%v/addtocart?ref=%v&product_id=%v&quantity=%v", URL, ref, product_id, quantity)
+	response, err := http.Post(request_url, "application/json", nil)
+	helpers.HandleTestError("httpPostError", err, t)
+
+	json_response := map[string]string{}
+	json.NewDecoder(response.Body).Decode(&json_response)
+
+	return json_response
+}
+
+func TestAddToCart(t *testing.T) {
+	test_cases := []map[string]any{
+		//valid reference, valid product_id and quantity available in inventory, add new item to cart
+		{"ref": "4d4d8297-7663-451d-b79e-49a545728552", "product_id": 3, "quantity": 1, "expected_response": "Item was added to the cart"},
+
+		//same as above, but increasing quantity of existing cart_item
+		{"ref": "4d4d8297-7663-451d-b79e-49a545728552", "product_id": 3, "quantity": 3, "expected_response": "Item was added to the cart"},
+
+		//quantity more than inventory stock
+		{"ref": "4d4d8297-7663-451d-b79e-49a545728552", "product_id": 3, "quantity": 200, "expected_response": "Inventory Quantity is less than the required quantity: 96"},
+
+		//Invalid Cart reference
+		{"ref": "4abcd", "product_id": 3, "quantity": 1, "expected_response": "Invalid cart_reference"},
+
+		//invalid product_id
+		{"ref": "4d4d8297-7663-451d-b79e-49a545728552", "product_id": 100, "quantity": 1, "expected_response": "Product id is invalid"},
+	}
+
+	for _, v := range test_cases {
+		response := AddToCartViaAPI(v["ref"].(string), v["product_id"].(int), v["quantity"].(int), t)
+
+		if response["message"] != v["expected_response"] {
+			t.Errorf("Expected: %v, Got: %v", v["expected_response"], response["message"])
+		}
+	}
+
+	RestoreDBTestingState(t)
+}
