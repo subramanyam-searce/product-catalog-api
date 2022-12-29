45363ca59d30d5c5758075753fa19d97f0adf0c2
Subramanyam R <subramanyam.r@searce.com>
2022-12-29 13:35:02 +0530

    Add log file

--- helpers/HandleError.go
+++ helpers/HandleError.go
@@ -2,10 +2,22 @@ package helpers
 
 import (
 	"fmt"
+	"log"
+	"os"
 )
 
 func HandleError(errorString string, err error) {
 	if err != nil {
-		fmt.Println(errorString+":", err)
+		output := fmt.Sprint(errorString+":", err)
+		fmt.Println(output)
+
+		file, err := os.OpenFile("logs.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer file.Close()
+
+		log.SetOutput(file)
+		log.Println(output)
 	}
 }
